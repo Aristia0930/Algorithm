9c63f078cdcb5f4f700874f1d160f977f37530e3
Aristia0930 <165614389+Aristia0930@users.noreply.github.com>
2025-02-04 23:05:01 +0900

    [Gold IV] Title: 합이 0, Time: 7832 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -1,39 +1,47 @@
-from sys import stdin
-# 이분탐색
+import sys
 
-n = int(stdin.readline())
-arr = list(map(int, stdin.readline().split()))
-arr.sort()
-ans = 0
+input = sys.stdin.readline
 
 
-def solve(s, e, g):
-    global ans
-    max_idx = n
-    while s < e:
-        tmp = arr[s] + arr[e]
-        if tmp < goal:
-            s += 1
+def count_zero_sum_triplets(n, array):
+    array.sort()
+    count = 0
 
-        elif tmp == g:
-            if arr[s] == arr[e]:
-                ans += e - s
+    def two_pointer(start):
+        nonlocal count
+        left, right = start + 1, n - 1
+        target = -array[start]
+        max_idx = n
 
-            else:
-                if max_idx > e:
-                    max_idx = e
-                    while max_idx >= 0 and arr[max_idx - 1] == arr[e]:
-                        max_idx -= 1
-                ans += e - max_idx + 1
-            s += 1
-        else:
-            e -= 1
+        while left < right:
+            sum_val = array[left] + array[right]
+
+            if sum_val < target:
+                left += 1
+            elif sum_val > target:
+                right -= 1
+            else:  # sum_val == target
+                if array[left] == array[right]:  # 같은 값일 경우
+                    count += right - left
+                else:
+                    if max_idx > right:
+                        max_idx = right
+                        while max_idx > 0 and array[max_idx - 1] == array[right]:
+                            max_idx -= 1
+
+                    count += right - max_idx + 1
+
+                left += 1
+
+    for i in range(n - 2):
+        two_pointer(i)
+
+    return count
 
 
-for i in range(n - 2):
-    start = i + 1
-    end = n - 1
-    goal = -arr[i]
-    solve(start, end, goal)
+# 입력 처리
+n = int(input())
+array = list(map(int, input().split()))
 
-print(ans)
+# 결과 출력
+print(count_zero_sum_triplets(n, array))
