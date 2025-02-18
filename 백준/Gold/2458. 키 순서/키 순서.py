ea0cf5b1f531adfd8a52093f2e9264463ddb16b7
Aristia0930 <165614389+Aristia0930@users.noreply.github.com>
2025-02-18 12:41:32 +0900

    [Gold IV] Title: 키 순서, Time: 4272 ms, Memory: 36144 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,28 @@
+def dfs(start,v,g):
+    global count
+    count+=1
+    v[start]=True
+    for i in g[start]:
+        if v[i]==False:
+            dfs(i,v,g)
+
+n,m=map(int,input().split())
+
+arr=[[] for _ in range(n+1)]
+arr2=[[] for _ in range(n+1)]
+
+for i in range(m):
+    a,b=map(int,input().split())
+    arr2[a].append(b)
+    arr[b].append(a)
+
+ans=0
+for i in range(1,n+1):
+    count=0
+    v=[False for _ in range(n+1)]
+    dfs(i,v,arr)
+    dfs(i,v,arr2)
+
+    if(count==n+1):
+        ans+=1
+print(ans)
