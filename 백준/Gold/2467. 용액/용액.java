06e7dfd0de9ae05492b13a0f65d685d91f32bf7b
Aristia0930 <165614389+Aristia0930@users.noreply.github.com>
2025-05-19 12:54:25 +0900

    [Gold V] Title: 용액, Time: 340 ms, Memory: 29224 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,61 +1,47 @@
-
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
 	
-
-
-
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st=new StringTokenizer(br.readLine());
-    	
-    	int n=Integer.parseInt(st.nextToken());
-    	Long[] arr = new Long[n];
-    	
-    	st=new StringTokenizer(br.readLine());
-    	for(int i=0; i<n;i++) {
-    		arr[i]=Long.parseLong(st.nextToken());
-    	}
-    	
-
-    	Arrays.sort(arr);
-    	Long min_sum=Long.MAX_VALUE;
-    	Long n1=0L;
-    	Long n2=0L;
-        int left = 0;
-        int right = n - 1;
-        
-        while(left<right) {
-        	long sum=arr[left]+arr[right];
-        	long absSum = Math.abs(sum);
-        	if(absSum<min_sum) {
-        		min_sum=absSum;
-        		n1=arr[left];
-        		n2=arr[right];
-        		
-        	}
-        	
-        	if(sum<0) {
-        		left++;
-        	}else {
-        		right--;
-        	}
-        	
-        }
-    	
-
-    	System.out.println(n1+" "+n2);
-    	
-
-
-    	
-    	
-    	
-    
-    }
-
-
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int N, minLeft, minRight;
+	static long minSum = Long.MAX_VALUE;
+	static long[] liquids;
+	
+	public static void main(String[] args) throws IOException {
+		
+		N = Integer.parseInt(br.readLine());
+		liquids = new long[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			liquids[i] = Long.parseLong(st.nextToken());
+		}
+		
+		int left = 0;
+		int right = N - 1;
+		
+		while (left < right) {
+			long sum = liquids[left] + liquids[right];
+			
+			if (Math.abs(sum) < minSum) {
+				minSum = Math.abs(sum);
+				minLeft = left;
+				minRight = right;
+			}
+			
+			if (sum > 0) {
+				right--;
+			} else if (sum < 0) {
+				left++;
+			} else {
+				break;
+			}
+		}
+		
+		bw.write(liquids[minLeft] + " " + liquids[minRight]);
+		bw.flush();
+	}
 }
