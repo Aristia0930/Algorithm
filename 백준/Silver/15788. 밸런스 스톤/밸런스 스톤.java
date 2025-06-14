43317566434e73e4ba0160f3b5ffd824063ec389
Aristia0930 <165614389+Aristia0930@users.noreply.github.com>
2025-06-15 02:38:42 +0900

    [Silver IV] Title: 밸런스 스톤, Time: 364 ms, Memory: 38220 KB -BaekjoonHub

--- 백준/Silver/15788. 밸런스 스톤/밸런스 스톤.java
+++ 백준/Silver/15788. 밸런스 스톤/밸런스 스톤.java
@@ -1,94 +1,97 @@
 
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, y, x;
-    static long[][] map;
-    static long s, sum;
+	
 
-    public static void main(String[] args) throws Exception {
-        input();
-        calcM();
 
-        if (checkLeftCross() && checkRightCross() && checkRow() && checkCol()) {
-            System.out.println(map[y][x]);
-        } else {
-            System.out.println(-1);
-        }
-    }
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st=new StringTokenizer(br.readLine());
+    	
+    	int n = Integer.parseInt(st.nextToken());
+    	long[][] array=new long[n][n];
+    	int x=-1;
+    	int y=-1;
+    	
+    	for(int i=0;i<n;i++) {
+    		st=new StringTokenizer(br.readLine());
+    		for(int j=0; j<n;j++) {
+    			int m=Integer.parseInt(st.nextToken());
+    			array[i][j]=m;
+    			if(m==0) {
+    				x=i;
+    				y=j;
+    			}
+    		}
+    		
+    		
+    	}
+    	long  t=0l;
+    	long  sum=0l;
+    	
+    	int find_x =  (x==0) ? 1:0;
+    	
+    	for(int i=0; i<n;i++) {
+    		t+=array[find_x][i];
+    	}
+    	for(int i=0; i<n;i++) {
+    		sum+=array[x][i];
+    	}
+    	
+    	long rs=t-sum;
+    	
+    	array[x][y]=rs;
+    	
+    	//행 열체크
 
-    static void input() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new long[N][N];
+    	for(int i=0 ;i <n;i++) {
+        	long col=0l;
+        	long row=0l;
+    		for(int j=0;j<n;j++) {
+    			row+=array[i][j];
+    			col+=array[j][i];
+    		}
+        	if(col!=t) {
+        		System.out.println(-1);
+        		return;
+        	}
+        	if(row!=t) {
+        		System.out.println(-1);
+        		return;
+        	}
+    	}
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Long.parseLong(st.nextToken());
-                if (map[i][j] == 0) {
-                    y = i;
-                    x = j;
-                }
-            }
-        }
-    }
+    	
+    	//대각선 체크
+    	long d1=0l;
+    	long d2=0l;
+    	for(int i=0;i<n;i++) {
+    		d1+=array[i][i];
+    		d2+=array[i][n-1-i];
+    	}
+    	
+    	if(d1!=t) {
+    		System.out.println(-1);
+    		return;
+    	}
 
-    static void calcM() {
-        // 기준 합 구하기
-        for (int i = 0; i < 2; i++) {
-            long tempSum = 0;
-            for (int j = 0; j < N; j++) {
-                tempSum += map[i][j];
-            }
-            sum = Math.max(sum, tempSum);
-        }
+       	if(d2!=t) {
+    		System.out.println(-1);
+    		return;
+    	}
+       	
+       	System.out.println(rs);
+    	
+    	
+    	
 
-        // 비어있는 줄의 합
-        s = 0;
-        for (int i = 0; i < N; i++) {
-            s += map[y][i];
-        }
+    	
+  
+    	
 
-        map[y][x] = sum - s;
-    }
 
-    static boolean checkLeftCross() {
-        s = 0;
-        for (int i = 0; i < N; i++) {
-            s += map[i][i];
-        }
-        return s == sum;
-    }
-
-    static boolean checkRightCross() {
-        s = 0;
-        for (int i = 0; i < N; i++) {
-            s += map[N - 1 - i][i];
-        }
-        return s == sum;
-    }
-
-    static boolean checkRow() {
-        for (int i = 0; i < N; i++) {
-            s = 0;
-            for (int j = 0; j < N; j++) {
-                s += map[i][j];
-            }
-            if (s != sum) return false;
-        }
-        return true;
-    }
-
-    static boolean checkCol() {
-        for (int i = 0; i < N; i++) {
-            s = 0;
-            for (int j = 0; j < N; j++) {
-                s += map[j][i];
-            }
-            if (s != sum) return false;
-        }
-        return true;
     }
 }
